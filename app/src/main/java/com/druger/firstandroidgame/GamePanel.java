a3eed334de529f29e4d1880e607e9c4d7e1f36de
druger <bayt32@yandex.ru>
2015-08-02 20:21:41 +0300

    Background created

--- app/src/main/java/com/druger/firstandroidgame/GamePanel.java
+++ app/src/main/java/com/druger/firstandroidgame/GamePanel.java
@@ -1,6 +1,8 @@
 package com.druger.firstandroidgame;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -10,7 +12,10 @@ import android.view.SurfaceView;
  */
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
+    public static final int WIDTH = 856;
+    public static final int HEIGHT = 480;
     private MainThread thread;
+    private Background bg;
 
     public GamePanel(Context context) {
         super(context);
@@ -26,6 +31,8 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        bg = new Background(BitmapFactory.decodeResource(getResources(), R.drawable.grassbg1));
+        bg.setVector(-5);
         //we can safely start the game loop
         thread.setRunning(true);
         thread.start();
@@ -58,6 +65,18 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     public void update(){
+        bg.update();
+    }
 
+    @Override
+    public void draw(Canvas canvas) {
+        final float scaleFactorX = getWidth() / WIDTH;
+        final float scaleFactorY = getHeight() / HEIGHT;
+        if (canvas != null) {
+            final int savedState = canvas.save();
+            canvas.scale(scaleFactorX, scaleFactorY);
+            bg.draw(canvas);
+            canvas.restoreToCount(savedState);
+        }
     }
 }
